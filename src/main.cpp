f0f74d283f90fb08fb2a70f72d0678f2ee70634d
Ludovic Hansen <ludovic.hansen@hotmail.be>
2024-09-18 00:17:47 +0200

    Basic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using std::cout;
-using std::cin;
-using std::endl;
+using std::string;
+using std::vector;
 
 auto main(int argc, char* argv[]) -> int
 {
+    size_t const size = argc;
+    vector<char*> arguments(argv, argv + argc);
+
+    if (argc < 1)
+    {
+        cout << "usage : echo <string you want to print>\n";
+    }
     
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arguments.at(i) << " ";
+    }
+
+    cout << "\n";
+
     return EXIT_SUCCESS;
 }
