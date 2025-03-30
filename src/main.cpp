902e6c10c0be9b747b541627f609fbc5872f415b
Ludovic Hansen <ludovic.hansen@hotmail.be>
2025-03-30 01:35:58 +0100

    Finish implementing

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,60 @@
 #include <string>
 #include <vector>
 
+#include <unistd.h>
+
+using std::cerr;
 using std::cout;
 using std::string;
 using std::vector;
 
 auto main(int argc, char* argv[]) -> int
 {
-    size_t const size = argc;
-    vector<char*> arguments(argv, argv + argc);
+    vector<char*> arguments(argv, argv + argc); // Create a vector of arguments from command-line inputs
+    int opt           = 0;                      // Variable to store the option returned by getopt
+    bool newLine      = true;                   // Flag to determine if a newline should be printed at the end
 
-    if (argc < 1)
+    // Check if no arguments are provided (argc <= 1 means no input string)
+    if (argc <= 1)
     {
-        cout << "usage : echo <string you want to print>\n";
-    }
-    
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arguments.at(i) << " ";
+        cerr << "Usage: ./echo [-n] <string>\n";
+        return EXIT_FAILURE;
     }
 
-    cout << "\n";
+    // Process the command-line options (e.g., "-n" flag) *
+    // Loop through options using getopt
+    while ((opt = getopt(argc, argv, "n")) != -1)
+    {
+        // If the -n flag is found, disable printing a newline at the end,
+        // if the flag is not -n, prints an error
+        switch (opt)
+        {
+        case 'n':
+            newLine = false;
+            break;
+        case '?':
+            cerr << "Valid option is -n\n";
+            return EXIT_FAILURE;
+        }
+    }
+
+    // Output the strings (arguments) passed after the options
+    // optind points to the first non-option argument
+    for (int i = optind; i < argc; i++)
+    {
+        cout << arguments.at(i);
+
+        if (i < argc - 1)
+        {
+            cout << " ";
+        }
+    }
+
+    // Print a newline if the -n flag was not used
+    if (newLine)
+    {
+        cout << "\n";
+    }
 
     return EXIT_SUCCESS;
 }
