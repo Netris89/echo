72cae3458864ae8d7387a98d9ad173f3beef82c9
Ludovic Hansen <ludovic.hansen@hotmail.be>
2025-05-05 20:25:33 +0200

    Add copyright to all files

--- source/parser.cpp
+++ source/parser.cpp
@@ -1,4 +1,30 @@
-#include <cctype>
+/*
+ *  Copyright (c) 2025, Ludovic Hansen
+ *  License: MIT
+ *
+ *  Description:
+ *  This application implements a custom version of the `echo` command in C++.
+ *  It handles various escape sequences, such as \a (alert), \b (backspace),
+ *  \n (newline), \t (tab), etc. It prints the command-line arguments with the
+ *  escape sequences interpreted correctly.
+ *
+ *  Usage: ./echo <string>
+ *
+ *  The following escape sequences are supported:
+ *    \a      : Alert
+ *    \b      : Backspace
+ *    \n      : Newline
+ *    \t      : Tab
+ *    \r      : Carriage return
+ *    \v      : Vertical tab
+ *    \\      : Backslash
+ *    \0num   : 8-bit octal value (num)
+ *
+ *  Note: More details on options and behavior can be found here:
+ *  https://pubs.opengroup.org/onlinepubs/9799919799/utilities/echo.html
+ */
+
+ #include <cctype>
 #include <cstddef>
 #include <string>
 
