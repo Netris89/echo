cd2018e78afa75c773dac5e33d4aa7515021a871
Ludovic Hansen <ludovic.hansen@hotmail.be>
2025-05-15 22:27:06 +0200

    Reorder tests

--- test/testParser.cpp
+++ test/testParser.cpp
@@ -1,26 +1,15 @@
 #include <gtest/gtest.h>
+#include <string>
 
 #include "parser.hpp"
 
+using std::string;
+
 TEST(ParserTests, TestSimpleArguments)
 {
-    EXPECT_EQ(Parser::ParseArgument("HelloWorld"), "HelloWorld");
-    EXPECT_EQ(Parser::ParseArgument("NoEscapeSequencesHere"), "NoEscapeSequencesHere");
-}
-
-TEST(ParserTests, TestNewLine)
-{
-    EXPECT_EQ(Parser::ParseArgument("Hello\nWorld"), "Hello\nWorld");
-}
-
-TEST(ParserTests, TestTab)
-{
-    EXPECT_EQ(Parser::ParseArgument("Tab\tSpace"), "Tab\tSpace");
-}
-
-TEST(ParserTests, TestBackspace)
-{
-    EXPECT_EQ(Parser::ParseArgument("Back\bSpace"), "Back\bSpace");
+    string testedString = Parser::ParseArgument("NoEscapeSequencesHere");
+    EXPECT_EQ(testedString, "NoEscapeSequencesHere");
+    EXPECT_EQ(testedString.size(), 21);
 }
 
 TEST(ParserTests, TestAlert)
@@ -28,21 +17,41 @@ TEST(ParserTests, TestAlert)
     EXPECT_EQ(Parser::ParseArgument("Alert\aSound"), "Alert\aSound");
 }
 
-TEST(ParserTests, TestLineBreak)
+TEST(ParserTests, TestBackspace)
 {
-    EXPECT_EQ(Parser::ParseArgument("Line\012Break"), "Line\nBreak");
+    EXPECT_EQ(Parser::ParseArgument("Back\bSpace"), "Back\bSpace");
+}
+
+TEST(ParserTests, TestSuppressNewLine)
+{
+    EXPECT_EQ(Parser::ParseArgument("Suppress\\cNewline"), "Suppress");
+}
+
+TEST(ParserTests, TestFormFeed)
+{
+    EXPECT_EQ(Parser::ParseArgument("Form\fFeed"), "Form\fFeed");
+}
+
+TEST(ParserTests, TestNewLine)
+{
+    EXPECT_EQ(Parser::ParseArgument("New\nLine"), "New\nLine");
+}
+
+TEST(ParserTests, TestTab)
+{
+    EXPECT_EQ(Parser::ParseArgument("Tab\tSpace"), "Tab\tSpace");
 }
 
 TEST(ParserTests, TestBackslash)
 {
-    EXPECT_EQ(Parser::ParseArgument("Backslash\\\\"), "Backslash\\\\");
+    EXPECT_EQ(Parser::ParseArgument("Back\\slash"), "Back\\slash");
+    EXPECT_EQ(Parser::ParseArgument("EndWith\\"), "EndWith\\");
+    EXPECT_EQ(Parser::ParseArgument("\\StartWith"), "\\StartWith");
 }
 
 TEST(ParserTests, TestInvalidEscapeSequences)
 {
     EXPECT_EQ(Parser::ParseArgument("Invalid\\xEscape"), "Invalid\\xEscape");
-    EXPECT_EQ(Parser::ParseArgument("EndWith\\"), "EndWith\\");
-    EXPECT_EQ(Parser::ParseArgument("\\StartWith"), "\\StartWith");
 }
 
 TEST(ParserTests, TestEmptyString)
@@ -52,5 +61,8 @@ TEST(ParserTests, TestEmptyString)
 
 TEST(ParserTests, TestOctal)
 {
-    EXPECT_EQ(Parser::ParseArgument("\041"), "!");
+    EXPECT_EQ(Parser::ParseArgument("\\041"), "!");
+    EXPECT_EQ(Parser::ParseArgument("\\012"), "\n");
+    EXPECT_EQ(Parser::ParseArgument("\\078"), "\0078"); // FIXME : Find why those tests don't pass
+    EXPECT_EQ(Parser::ParseArgument("\\0x"), "\0x");    // FIXME : Find why those tests don't pass
 }
