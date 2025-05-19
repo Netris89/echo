2a2434c68fe970af0f0468acc7cbe43116a42014
Ludovic Hansen <ludovic.hansen@hotmail.be>
2025-05-19 20:33:40 +0200

    Make tests names more readable

--- test/testParser.cpp
+++ test/testParser.cpp
@@ -5,34 +5,34 @@
 
 using std::string;
 
-TEST(ParserTests, TestSimpleArguments)
+TEST(ParserTests, PlainText)
 {
     string testedString = Parser::ParseArgument("NoEscapeSequencesHere");
     EXPECT_EQ(testedString, "NoEscapeSequencesHere");
     EXPECT_EQ(testedString.size(), 21);
 }
 
-TEST(ParserTests, TestAlert)
+TEST(ParserTests, AlertSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Alert\aSound"), "Alert\aSound");
 }
 
-TEST(ParserTests, TestBackspace)
+TEST(ParserTests, BackspaceSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Back\bSpace"), "Back\bSpace");
 }
 
-TEST(ParserTests, TestSuppressNewLine)
+TEST(ParserTests, SuppressNewline)
 {
     EXPECT_EQ(Parser::ParseArgument("Suppress\\cNewline"), "Suppress");
 }
 
-TEST(ParserTests, TestFormFeed)
+TEST(ParserTests, FormFeedSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Form\fFeed"), "Form\fFeed");
 }
 
-TEST(ParserTests, TestNewLine)
+TEST(ParserTests, NewlineSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("New\nLine"), "New\nLine");
 }
@@ -42,34 +42,34 @@ TEST(ParserTests, TestTab)
     EXPECT_EQ(Parser::ParseArgument("Tab\tSpace"), "Tab\tSpace");
 }
 
-TEST(ParserTests, TestCarriageReturn)
+TEST(ParserTests, CarriageReturnSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Carriage\rReturn"), "Carriage\rReturn");
 }
 
-TEST(ParserTests, TestVerticalTab)
+TEST(ParserTests, VerticalTabSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Vertical\vTab"), "Vertical\vTab");
 }
 
-TEST(ParserTests, TestBackslash)
+TEST(ParserTests, BackslashSequence)
 {
     EXPECT_EQ(Parser::ParseArgument("Back\\slash"), "Back\\slash");
     EXPECT_EQ(Parser::ParseArgument("EndWith\\"), "EndWith\\");
     EXPECT_EQ(Parser::ParseArgument("\\StartWith"), "\\StartWith");
 }
 
-TEST(ParserTests, TestInvalidEscapeSequences)
+TEST(ParserTests, InvalidEscape)
 {
     EXPECT_EQ(Parser::ParseArgument("Invalid\\xEscape"), "Invalid\\xEscape");
 }
 
-TEST(ParserTests, TestEmptyString)
+TEST(ParserTests, EmptyInput)
 {
     EXPECT_EQ(Parser::ParseArgument(""), "");
 }
 
-TEST(ParserTests, TestOctal)
+TEST(ParserTests, OctalEscape)
 {
     EXPECT_EQ(Parser::ParseArgument("\\041"), "!");
     EXPECT_EQ(Parser::ParseArgument("\\012"), "\n");
@@ -77,13 +77,13 @@ TEST(ParserTests, TestOctal)
     EXPECT_EQ(Parser::ParseArgument("\\0x"), "\\0x");
 }
 
-TEST(ParserTests, TestNoOption)
+TEST(ParserTests, CommandLineOptionLikeInput)
 {
     EXPECT_EQ(Parser::ParseArgument("-eEcho"), "-eEcho");
     EXPECT_EQ(Parser::ParseArgument("-nNoNewline"), "-nNoNewline");
 }
 
-TEST(ParserTests, TestMultipleEscapeSequences)
+TEST(ParserTests, MixedEscapeSequences)
 {
     EXPECT_EQ(Parser::ParseArgument("Mix\\a\\b\\t\\nEnd"), string("Mix") + '\a' + '\b' + '\t' + '\n' + "End");
 }
